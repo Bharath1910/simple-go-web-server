403740b3856d09f5ebf07239dd6df840aa7f03ce
Bharath1910 <av.bharath@proton.me>
2023-02-20 15:24:12 +0530

    Completed create helloHandler TODO: 1. Create formHandler

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.Handle("/form", formHandler)
 	http.Handle("/hello", helloHandler)
-}
 
-func formHandler() {
+	fmt.Println("Starting server at port 8080")
 
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
