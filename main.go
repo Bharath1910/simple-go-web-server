1fffb4b18bfc79a65e6bf4ef2a891d9cc13a2482
Bharath1910 <av.bharath@proton.me>
2023-02-20 15:16:34 +0530

    completed 3 routes TODO: 1. Create formHandler 2. Create helloHandler

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.Handle("/form", formHandler)
+	http.Handle("/hello", helloHandler)
+}
+
+func formHandler() {
 
 }
